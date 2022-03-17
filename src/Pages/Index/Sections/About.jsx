b5917944e63de023e92415114f0c5d42f9a6336d
Gael Perez <gael.perez@bestowinc.com>
2022-03-16 21:02:18 -0600

    Refactor to avoid breaks

--- src/Pages/Index/Sections/About.jsx
+++ src/Pages/Index/Sections/About.jsx
@@ -31,7 +31,7 @@ export default class About extends Component{
               <div class="gallery__column">
               <a class="gallery__link">
                   <figure class="gallery__thumb">
-                    <img src={Emi} class="gallery__image" data-scroll-hide="false" data-multiline="true" data-tip="Estudiante de 4to semestre en Ingeniería en Tecnologías Computacionales. <br /> Interés por la programación, web development y data analytics. <br />Mis hobbies son jugar videojuegos, aprender idiomas, gastronomía, <br />viajar, escuchar música  y practicar yoga." />
+                    <img src={Emi} class="gallery__image"  data-multiline="true" data-tip="Estudiante de 4to semestre en Ingeniería en Tecnologías Computacionales. <br /> Interés por la programación, web development y data analytics. <br />Mis hobbies son jugar videojuegos, aprender idiomas, gastronomía, <br />viajar, escuchar música  y practicar yoga." />
                     <figcaption class="gallery__caption">Emiliano Saucedo Arriola</figcaption>
                   </figure>
                 </a>
@@ -39,7 +39,7 @@ export default class About extends Component{
               <div class="gallery__column">
                 <a class="gallery__link">
                   <figure class="gallery__thumb">
-                    <img src={Gael} class="gallery__image" data-scroll-hide="false" data-multiline="true" data-tip="Me llamo Gael Eduardo Pérez Gómez y soy estudiante de la carrera ITC <br /> en el Tecnológico de Monterrey. Soy desarrollador Fullstack con 3 años  <br /> de experiencia y técnico en programación del IPN. En mis tiempos libres  <br />me gusta mucho aprender sobre nuevas tecnologías, salir con mis amigos o ir a  <br /> fiestas, amo la música y sé tocar el bajo y la guitarra." />
+                    <img src={Gael} class="gallery__image"  data-multiline="true" data-tip="Me llamo Gael Eduardo Pérez Gómez y soy estudiante de la carrera ITC <br /> en el Tecnológico de Monterrey. Soy desarrollador Fullstack con 3 años  <br /> de experiencia y técnico en programación del IPN. En mis tiempos libres  <br />me gusta mucho aprender sobre nuevas tecnologías, salir con mis amigos o ir a  <br /> fiestas, amo la música y sé tocar el bajo y la guitarra." />
                     <ReactTooltip />
                     <figcaption class="gallery__caption">Gael Eduardo Pérez Gómez</figcaption>
                   </figure>
@@ -48,7 +48,7 @@ export default class About extends Component{
               <div class="gallery__column">
                 <a class="gallery__link">
                   <figure class="gallery__thumb">
-                    <img src={Alex} class="gallery__image"  data-scroll-hide="false" data-multiline="true" data-tip="Mi nombre es Alejandro Díaz Villagómez, tengo 19 años y actualmente vivo en la <br /> Ciudad de México. Estoy cursando mi 4to semestre en la carrera de <br /> Ingeniería en Tecnologías Computacionales (ITC) en el Tecnológico de Monterrey <br /> Campus Ciudad de México. Me encanta el fútbol, la tecnología y pasar <br /> tiempo con familiares y amigos." />
+                    <img src={Alex} class="gallery__image"   data-multiline="true" data-tip="Mi nombre es Alejandro Díaz Villagómez, tengo 19 años y actualmente vivo en la <br /> Ciudad de México. Estoy cursando mi 4to semestre en la carrera de <br /> Ingeniería en Tecnologías Computacionales (ITC) en el Tecnológico de Monterrey <br /> Campus Ciudad de México. Me encanta el fútbol, la tecnología y pasar <br /> tiempo con familiares y amigos." />
                     <figcaption class="gallery__caption">Alejandro Díaz Villagómez</figcaption>
                   </figure>
                 </a>
@@ -56,7 +56,7 @@ export default class About extends Component{
               <div class="gallery__column">
                 <a class="gallery__link">
                   <figure class="gallery__thumb">
-                    <img src={Poncho} class="gallery__image" data-scroll-hide="false" data-multiline="true" data-tip="Mi nombre es Alfonso Pineda, tengo 19 años y estudio desarrollo de software. <br />  Me gusta invertir mi tiempo libre en la fotografía y video; además <br />  de ver deportes de automovilismo, la Fórmula 1 es mi favorito. <br />  Me considero una persona comprometida, honesta y dedicada." />
+                    <img src={Poncho} class="gallery__image"  data-multiline="true" data-tip="Mi nombre es Alfonso Pineda, tengo 19 años y estudio desarrollo de software. <br />  Me gusta invertir mi tiempo libre en la fotografía y video; además <br />  de ver deportes de automovilismo, la Fórmula 1 es mi favorito. <br />  Me considero una persona comprometida, honesta y dedicada." />
                     <figcaption class="gallery__caption">Alfonso Pineda</figcaption>
                   </figure>
                 </a>
